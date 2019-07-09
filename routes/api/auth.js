85ca411833fc41436be71036ae240a1723b95fc8
Divya Kumar <divya.kumar@learningmatemumbai.local>
2019-07-09 19:07:49 +0530

    v13

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 //@route  get api/auth
 //@desc Test route
 //@access Public
 
-router.get("/", (req, res) => res.send("auth route"));
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User
+    } catch (err) {
+
+    }
+}
+);
 
 module.exports = router;
